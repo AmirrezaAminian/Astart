c29513dfa4f11e84e215a93ae00fc83a8736971a
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-06-15 02:30:22 +0430

    add p5 libraries

---
+++ script.js
@@ -0,0 +1,27 @@
+
+var cols = 5 ;
+var rows = 5 ;
+var grid = new Array();
+
+function setup(){
+    createCanvas(400 , 400) ;
+    console.log('A*') ;
+
+}
+
+// Making a 2D array
+   for(var i=0 ; i < cols ; i++){
+    const element = array[j];
+}
+    
+   for (let j = 0; j < rows; j++) {
+        
+    grid[i] = new Array(rows);
+}
+
+ console.log(grid);
+
+
+function draw(){
+    background(0) ;
+}
