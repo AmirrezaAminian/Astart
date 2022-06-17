07ece4338f744a3d8b0699c7c05a88bd7ef0aafc
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-06-17 23:27:03 +0430

    First step is done!

--- script.js
+++ script.js
@@ -4,16 +4,35 @@ var cols = 5 ;
 var rows = 5 ;
 var grid = new Array(cols);
 
+var openSet = [] ;
+var closedSet = [] ;
+var start ;
+var end;
+var w , h ;
 
-  function Spot(){
+
+
+  function Spot( i ,j){
+    this.x =i ;
+    this.y = j ; 
     this.f = 0 ;
     this.g = 0 ;
     this.h = 0 ;
+
+    this.show = function(col){
+      fill(255) ;
+      //stroke(0);
+      noStroke();
+      rect(this.x*w , this.y*h , w - 1 , h - 1)
+    }
   }
 
 function setup(){
   createCanvas(400, 400);
   console.log("A*");
+
+  w = width / cols ;
+  h = height / rows ;
 
   // Making a 2D Array 
   for (var i = 0; i < cols; i++) {
@@ -22,15 +41,34 @@ function setup(){
 
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
-      grid[i][j] = new Spot();
+      grid[i][j] = new Spot( i , j);
     }
   }
+
+  start = grid[0][0] ;
+  end = grid[cols - 1][rows - 1] ;
+
+  openSet.push(start) ;
+
+    
 
   console.log(grid) ;
 }
 
 function draw(){
+
+  if(openSet.length > 0){
+    // we can keep going
+  }else{
+    // ...
+  }
   background(0) ;
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].show() ;
+    }
+  }
 
 
 }
