547bb366e9dda367d9ebfe5eb36f7bc71c56e4b3
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-06-17 23:10:43 +0430

    lets add openSet && closedSet

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
+'use strict' ;
 
 var cols = 5 ;
 var rows = 5 ;
-var grid = new Array();
+var grid = new Array(cols);
+
+
+  function Spot(){
+    this.f = 0 ;
+    this.g = 0 ;
+    this.h = 0 ;
+  }
 
 function setup(){
-    createCanvas(400 , 400) ;
-    console.log('A*') ;
+  createCanvas(400, 400);
+  console.log("A*");
 
-}
-
-// Making a 2D array
-   for(var i=0 ; i < cols ; i++){
-    const element = array[j];
-}
-    
-   for (let j = 0; j < rows; j++) {
-        
+  // Making a 2D Array 
+  for (var i = 0; i < cols; i++) {
     grid[i] = new Array(rows);
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j] = new Spot();
+    }
+  }
+
+  console.log(grid) ;
 }
-
- console.log(grid);
-
 
 function draw(){
-    background(0) ;
+  background(0) ;
+
+
 }
