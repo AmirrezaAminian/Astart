f33c36339adea599be755b1cf58be754e3b1c887
Amirreza Aminian <aminian.amirreza@gmail.com>
2022-06-18 00:35:10 +0430

    each neighbor of current

--- script.js
+++ script.js
@@ -22,17 +22,39 @@ var w , h ;
 
 
   function Spot( i ,j){
-    this.x =i ;
-    this.y = j ; 
+    this.i =i ;
+    this.j = j ; 
     this.f = 0 ;
     this.g = 0 ;
     this.h = 0 ;
+    this.neighbors = [] ;
 
     this.show = function(col){
       fill(col) ;
       //stroke(0);
       noStroke();
-      rect(this.x*w , this.y*h , w - 1 , h - 1)
+      rect(this.i*w , this.j*h , w - 1 , h - 1)
+    }
+
+    this.addNeighbors = function(grid){
+      var i = this.i ;
+      var j = this.j ;
+
+      if(i < cols -1){
+        this.neighbors.push(grid[i + 1][j]);
+      }
+
+      if(i > 0){
+        this.neighbors.push(grid[i - 1][j]);
+      }
+
+      if(j < rows - 1){
+        this.neighbors.push(grid[i][j + 1]);
+      }
+
+      if(j > 0){
+        this.neighbors.push(grid[i][j - 1]);
+      }
     }
   }
 
@@ -51,6 +73,12 @@ function setup(){
   for (var i = 0; i < cols; i++) {
     for (var j = 0; j < rows; j++) {
       grid[i][j] = new Spot( i , j);
+    }
+  }
+
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid);
     }
   }
 
